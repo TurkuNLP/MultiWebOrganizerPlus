e7d9d4a5b3d34b3aec40f053152a18e98e077ff0
OttoTarkka <otto.tarkka@gmail.com>
2026-09-04 15:02:30 +0300

    Reserve 4 GPUs, change config

--- hpc/lumi/discover_topic_labels.sh
+++ hpc/lumi/discover_topic_labels.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=discover_topic_labels
 #SBATCH --account=project_462001516
-#SBATCH --partition=dev-g
-#SBATCH --time=00:45:00
+#SBATCH --partition=small-g
+#SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=120G
-#SBATCH --gpus-per-node=2
+#SBATCH --gpus-per-node=4
 #SBATCH -o ../logs/discover_%j.out
 #SBATCH -e ../logs/discover_%j.err
 
@@ -24,7 +24,6 @@ SIF="$base_dir/environments/lumi/BASE.sif"
 python_script="$base_dir/scripts/label_pipeline.py"
 
 srun singularity run -B /scratch/project_462001516 $SIF python "$python_script" \
-                        --config "$base_dir/configs/pipeline_defaults.yaml" \
+                        --config "$base_dir/configs/full_run1.yaml" \
                         --mode discover \
                         --input "$base_dir/data/FineWebSample.jsonl" \
-                        --reset-discovery
